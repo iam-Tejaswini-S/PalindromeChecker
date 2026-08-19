b83519c11f4874b7f20d45f710f39468a6b99372
iam-Tejaswini-S <tejaswini.srini.2020@gmail.com>
2026-08-19 13:55:48 +0530

    Add palindrome checker program

---
+++ PalindromeChecker.java
@@ -0,0 +1,21 @@
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        System.out.println("Original Word: " + word);
+        System.out.println("Reversed Word: " + reversed);
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: The word is a Palindrome");
+        } else {
+            System.out.println("Result: The word is not a Palindrome");
+        }
+    }
+}
